23707203eff142c421eee008de4f77079d590da7
inzamulhaque <mdihalif@gmail.com>
2024-09-02 19:59:13 +0600

    prisma error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
@@ -7,10 +8,26 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: error.message || "Something went wrong!",
-    error,
+  let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = error.message || "Something went wrong!";
+  let err = error;
+
+  if (error instanceof Prisma.PrismaClientValidationError) {
+    statusCode = httpStatus.FORBIDDEN;
+    message = "Validation Error";
+    err = error.message;
+  } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === "P2002") {
+      message = "Duplicate Key Error";
+      err = error.meta;
+    }
+  }
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error: err,
   });
 };
 
