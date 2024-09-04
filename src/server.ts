c0d862bd98f8df866e37c77023a62bea1c2acbab
inzamulhaque <mdihalif@gmail.com>
2024-09-04 18:55:21 +0600

    handle unexpected error

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,25 @@ async function main() {
   const server: Server = app.listen(port, () => {
     console.log(`http://localhost:${port}`);
   });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.info("Server closed!");
+      });
+    }
+    process.exit(1);
+  };
+
+  process.on("uncaughtException", (error) => {
+    console.log(error);
+    exitHandler();
+  });
+
+  process.on("unhandledRejection", (error) => {
+    console.log(error);
+    exitHandler();
+  });
 }
 
 main();
